87a0719510ee9724b2626be49f4aa4d0f3bc9dfa
Jawan Higgins <jawanhiggins@jawans-mbp.attlocal.net>
2017-10-26 23:51:08 -0400

    Added Register and Login functionality

---
+++ app/src/main/java/familyconnect/familyconnect/Login.java
@@ -0,0 +1,123 @@
+package familyconnect.familyconnect;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+public class Login extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    private SignInButton buttonLogin;
+    private GoogleSignInResult result;
+    private String accountEmail;
+    private String realName;
+    private String displayName;
+    private String picURL;
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
+    private static int RC_SIGN_IN = 9001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        buttonLogin = (SignInButton) findViewById(R.id.sign_in_button);
+
+        buttonLogin.setOnClickListener(this);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */,  this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+    }
+
+
+    private void updateUI(boolean isLogin) {
+
+        if(isLogin) {
+            Intent welcomePage = new Intent(Login.this, UserMainArea.class);
+            Login.this.startActivity(welcomePage);
+        }
+    }
+
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            accountEmail = acct.getEmail();
+            realName = acct.getGivenName();
+            displayName = acct.getDisplayName();
+            picURL = acct.getPhotoUrl().toString();
+            String authCode = acct.getServerAuthCode();
+
+            sharedPreferences = getSharedPreferences("USER_ATTRIBUTES", MODE_PRIVATE);
+            editor = sharedPreferences.edit();
+            editor.putBoolean("isLogin", true);
+            editor.putString("name", realName);
+            editor.putString("email",accountEmail);
+            editor.putString("picture", picURL);
+            editor.commit();
+
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+}
---
+++ app/src/main/java/familyconnect/familyconnect/UserMainArea.java
@@ -0,0 +1,94 @@
+package familyconnect.familyconnect;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+
+public class UserMainArea extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+
+
+    private TextView name, email;
+    private ImageView picture;
+    private Button buttonLogout;
+    private GoogleApiClient mGoogleApiClient;
+    private SharedPreferences prefs;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usermainarea);
+
+        //Retrieve Data from GOOGLE Sign In
+        prefs = getSharedPreferences("USER_ATTRIBUTES", MODE_PRIVATE);
+
+        buttonLogout = (Button) findViewById(R.id.sign_out_button);
+        name = (TextView) findViewById(R.id.profileName);
+        name.setText(prefs.getString("name",""));
+
+        email = (TextView) findViewById(R.id.profileEmail);
+        email.setText(prefs.getString("email", ""));
+
+        picture = (ImageView) findViewById(R.id.profilePic);
+        Glide.with(this).load(prefs.getString("picture","")).into(picture);
+
+        buttonLogout.setOnClickListener(this);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */,  this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.sign_out_button:
+                signOut();
+                break;
+        }
+    }
+
+    private void signOut() {
+
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                updateUI(false);
+            }
+        });
+    }
+
+    private void updateUI(boolean isLogin) {
+
+        Intent signInPage = new Intent(UserMainArea.this, Login.class);
+        UserMainArea.this.startActivity(signInPage);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
